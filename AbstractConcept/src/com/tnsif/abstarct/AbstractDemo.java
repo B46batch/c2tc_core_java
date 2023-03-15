61be49d43a9ea6c13bfdd0891722a69dce4943fa
Hp <Hp@DESKTOP-MEHK842>
2023-03-15 14:32:15 +0545

    first commit

--- AbstractConcept/src/com/tnsif/abstarct/AbstractDemo.java
+++ AbstractConcept/src/com/tnsif/abstarct/AbstractDemo.java
@@ -3,6 +3,7 @@ package com.tnsif.abstarct;
 abstract class Animal
 {
   abstract void eat();
+  
 }
  class AbstractDemo extends Animal
 {
@@ -18,4 +19,9 @@ System.out.print("i am vegiterian");
 	}
 	public static void main(String args[])
 	{
-		Animal goat = new AbstractDemo();} }
+		Animal goat = new AbstractDemo();
+		goat.eat();
+		
+	}
+	}
+
